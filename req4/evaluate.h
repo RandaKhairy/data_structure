2cd40d09dd3450cec542615e64e22c38ccd7e94d
OmarSha3rawy <omar.aadel58@gmail>
2019-10-19 22:00:26 +0200

    adding comments to the code

--- req4/evaluate.h
+++ req4/evaluate.h
@@ -4,6 +4,17 @@
 #include"stack.h"
 #include "queue.h"
 
+/*
+ * Function:  evaluate
+ * --------------------
+ * computes a mathematical expression using stack to check balancing and Queues to evaluate it sequentially:
+ *
+ * expression: array of characters contains numbers and operators
+ *
+ * returns: the result of the expression
+ *
+ * restrictions : no precedence and the number should from 0 to 9
+ */
 long long evaluate(char* expression);
 
 #endif // __LINKED_LIST__
